55f5eed69be6d364ca4254f056f3c37d1db7da4e
Pascal Sauerborn <45198989+PSauerborn@users.noreply.github.com>
2019-03-24 17:02:22 +0000

    Create client.py

---
+++ client.py
@@ -0,0 +1,20 @@
+class RPCProxy():
+
+    def __init__(self, connection):
+        self._connection = connection
+
+    def __getattr__(self, name):
+
+        def do_rpc(*args, **kwargs):
+            self._connection.send(pickel.dumps((names, args, kwargs)))
+            results = pickle.loads(self._connection.revc())
+
+            if istinstance(result, Exception):
+                raise result
+            return result
+        return do_rpc
+
+# the proxy is essentially a wrapper for a client
+
+c = Client(('localhost', 17000), authkey=b'peekaboo')
+proxy = RPCProxy(c)
